cbf1c8f54b9144b0e32166b51cd6b4e250429eba
n0urs <20nourabdulnasser00@gmail.com>
2024-12-27 23:10:54 +0200

    implemted google sign in

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AUTH_CONFIG, AuthConfig } from 'auth-api';
@@ -16,6 +17,23 @@ export const appConfig: ApplicationConfig = {
       useValue: {
         baseUrl: environment.baseUrl, // Here's where environment connects to your library
       } as AuthConfig,
+    },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '526742432154-b1m53cefpc5h7hjc0man8t3grnkm1hhn.apps.googleusercontent.com'
+            ),
+          },
+        ],
+        onError: (error) => {
+          console.error(error);
+        },
+      } as SocialAuthServiceConfig,
     },
   ],
 };
--- src/app/core/layout/auth-layout/auth-layout.component.ts
+++ src/app/core/layout/auth-layout/auth-layout.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthNavBarComponent } from '../../../feature/components/auth-nav-bar/auth-nav-bar.component';
 import { SocialsListComponent } from '../../../shared/components/ui/socials-list/socials-list.component';
+import { CommonModule } from '@angular/common';
 import { DividerComponent } from '../../../shared/components/ui/divider/divider.component';
-
+import {
+  SocialAuthService,
+  GoogleSigninButtonModule,
+} from '@abacritt/angularx-social-login';
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, FormsModule, AuthNavBarComponent, SocialsListComponent, DividerComponent],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    FormsModule,
+    AuthNavBarComponent,
+    SocialsListComponent,
+    DividerComponent,
+    CommonModule,
+    GoogleSigninButtonModule,
+  ],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
-export class AuthLayoutComponent {
-
+export class AuthLayoutComponent implements OnInit {
+  constructor(
+    private _SocialAuthService: SocialAuthService,
+    private _router: Router
+  ) {}
+  ngOnInit(): void {
+    this._SocialAuthService.authState.subscribe({
+      next: (response) => {
+        if (response) {
+          localStorage.setItem('onlineExamsUser', response.idToken);
+          this._router.navigateByUrl('/home');
+        }
+      },
+    });
+  }
 }
